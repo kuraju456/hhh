db071de380c88693d7e18d769f2169cacc738a78
Raju kumar <108264219+kuraju456@users.noreply.github.com>
2023-10-18 22:01:21 +0530

    h
    hjh

---
+++ quicksell-assignment-main/quicksell-assignment-main/src/appp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DashView from "./components/DashBoard/DashView"; // Import the DashView component
+
+function App() {
+  return (
+    <div className="App">
+      {/* You can use the DashView component in your JSX like this */}
+      <DashView />
+    </div>
+  );
+}
+
+export default App;
